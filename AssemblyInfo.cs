c2a1a5071a072e440d67af35dbe4c59002b657ef
markbnorman-admin <mark95014@protonmail.com>
2026-04-06 14:59:53 -0400

    changed to use the NUnit syntax

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,1 +1,7 @@
-﻿[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)]
+﻿using NUnit.Framework;
+
+// Enable parallel execution at fixture level (test classes run in parallel)
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
+// Set number of workers (0 or omit = use all cores, or specify a number like 4)
+[assembly: LevelOfParallelism(0)]
