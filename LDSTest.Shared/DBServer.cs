bbcfb6ea0d30084d7a92c7af6c4d4071fef0d96c
markbnorman-admin <mark95014@protonmail.com>
2026-04-15 12:34:07 -0400

    working on database restore

---
+++ LDSTest.Shared/DBServer.cs
@@ -0,0 +1,9 @@
+﻿namespace LDSTest.Shared.utils
+{
+    public class DBServer(string serverName, string userName, string password)
+    {
+        public string serverName = serverName;
+        public string userName = userName;
+        public string password = password;
+    }
+}
